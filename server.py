1292dab5f8f28c74566f2872f53667b2c709d08e
Fdhlnn23 <169079071+Fdhlnn23@users.noreply.github.com>
2026-03-19 22:13:08 +0700

    Update server.py

--- server.py
+++ server.py
@@ -111,7 +111,8 @@ def download_video():
     else:
         opts.update({
             'format': (
-                f'bestvideo[height<={quality}]+bestaudio'
+                f'bestvideo[height<={quality}][ext=mp4]+bestaudio[ext=m4a]'
+                f'/bestvideo[height<={quality}]+bestaudio'
                 f'/best[height<={quality}]'
                 f'/best'
             ),
